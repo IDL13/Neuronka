a0203b9f5ac17f26cf4b5a9a22f48b3e4c3122d1
IDL13 <leonov.ilya.02@gmail.com>
2023-03-06 22:10:05 +0300

    first test

--- cmd/main.go
+++ cmd/main.go
@@ -12,9 +12,13 @@ func main() {
 	create.CreateNN()
 	nn := gonn.LoadNN("gonn")
 
-	var x float64 = 0.1
-	var y float64 = 0.1
+	var x1 float64 = 1.0
+	var y1 float64 = 5.0
+	var x2 float64 = 2.0
+	var y2 float64 = 4.0
+	var x3 float64 = 5.0
+	var y3 float64 = 5.0
 
-	out := nn.Forward([]float64{x, y})
+	out := nn.Forward([]float64{x1, y1, x2, y2, x3, y3})
 	fmt.Println(activation.Activation(out))
 }
--- internal/create/create.go
+++ internal/create/create.go
@@ -5,11 +5,15 @@ import (
 )
 
 func CreateNN() {
-	nn := gonn.DefaultNetwork(2, 16, 2, false)
+	nn := gonn.DefaultNetwork(6, 18, 2, false)
 
-	input := [][]float64{}
+	input := [][]float64{
+		[]float64{0.0, 0.0, 1.0, 1.0, 2.0, 2.0}, []float64{1.0, 1.0, 2.0, 2.0, 3.0, 1.0}, []float64{1.0, 1.0, 2.0, 2.0, 3.0, 3.0},
+		[]float64{2.0, 4.0, 3.0, 3.0, 4.0, 2.0}, []float64{1.0, 5.0, 2.0, 4.0, 3.0, 5.0}}
 
-	target := [][]float64{}
+	target := [][]float64{
+		[]float64{1, 0}, []float64{0, 1}, []float64{1, 0},
+		[]float64{0, 1}, []float64{1, 0}}
 
 	nn.Train(input, target, 10000)
 
